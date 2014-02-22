cbd1c3e65bcab306746305b49ecbff0daba98432
Thomas Harman <tharman@expedia.com>
2014-02-22 11:29:55 -0800

    Show James' fresh images

--- www/js/review.js
+++ www/js/review.js
@@ -1,18 +1,18 @@
 (function($){ // self invoking function
 
   var myImage= [];
-  myImage[0]="img/bad-1.jpg";
-  myImage[1]="img/bad-2.jpg";
-  myImage[2]="img/bad-3.jpg";
-  myImage[3]="img/good-1.jpg";
-  myImage[4]="img/good-2.jpg";
-  myImage[5]="img/good-3.jpg";
+  myImage[0]="img/review-images/bad-1.jpg";
+  myImage[1]="img/review-images/bad-2.jpg";
+  myImage[3]="img/review-images/good-1.jpg";
+  myImage[2]="img/review-images/bad-3.jpg";
+  myImage[4]="img/review-images/good-2.jpg";
+  myImage[5]="img/review-images/good-3.jpg";
   var imgCnt = 0;
   var $reviewimagebox = $("#review_image_box");
   var $remainingvotes = $("#remaining_votes");
 
   function showImg(imgSrc) {
-    $reviewimagebox.css('background', 'url(' + imgSrc + ')');
+    $reviewimagebox.css('background-image', 'url(' + imgSrc + ')');
   }
 
   function next(e){
